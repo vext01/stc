c553aba1a4ba3974e8f0fd1bb7ab37ea9eb283e1
vext01 <vext01@gmail.com>
2013-05-08 16:41:44 +0100

    Greatly simplify operator representation.
    Operators are num functions of the form x : Stack.t -> unit.
    
    Also don't dump the stack on every input and print the number of items
    in the stack in the prompt.

--- stc.ml
+++ stc.ml
@@ -5,12 +5,7 @@ open Hashtbl;;
 (* Map module for the operation table *)
 module StringMap = Map.Make(String);;
 
-(* Variant Types *)
-type op = UnaryOp of (Num.num -> Num.num)
-        | BinaryOp of (Num.num -> Num.num -> Num.num)
-        | TrinaryOp of (Num.num -> Num.num -> Num.num -> Num.num)
-        | SpecialOp of (Num.num Stack.t -> unit ) * int;;
-
+type op = Operation of (Num.num Stack.t -> unit);;
 type err = Parse_error | Stack_underflow;;
 
 (* Functions *)
@@ -30,47 +25,11 @@ let rec dump_stack stk =
         | _ -> let elem = Stack.pop stk in dump_stack stk;
                 Printf.printf "  %s\n" (string_of_num elem);
                 Stack.push elem stk;;
-
-let eval_unary_op stk f =
-        if (Stack.length stk) < 1 then
-                print_err Stack_underflow
-        else
-                let res = f (Stack.pop stk) in Stack.push res stk;;
-
-let eval_binary_op stk f = 
-        if (Stack.length stk) < 2 then
-                print_err Stack_underflow
-        else
-                let oprnd1 = Stack.pop stk in
-                let oprnd2 = Stack.pop stk in
-                let res = f oprnd2 oprnd1 in
-                Stack.push res stk;;
-
-let eval_trinary_op stk f =
-        if (Stack.length stk) < 3 then
-                print_err Stack_underflow
-        else
-                let oprnd1 = Stack.pop stk in
-                let oprnd2 = Stack.pop stk in
-                let oprnd3 = Stack.pop stk in
-                let res = f oprnd3 oprnd2 oprnd1 in
-                Stack.push res stk;;
-
-let eval_special_op stk f nargs = 
-        if (Stack.length stk) < nargs then
-                print_err Stack_underflow
-        else f stk;;
-
-let eval_op op stk = match op with
-        | UnaryOp f -> eval_unary_op stk f
-        | BinaryOp f -> eval_binary_op stk f
-        | TrinaryOp f -> eval_trinary_op stk f
-        | SpecialOp (f, nargs) -> eval_special_op stk f nargs;;
-
+ 
 (* second level parsing *)
 let parse_operator stk optab line =
         try let op = StringMap.find line optab in
-        eval_op op stk
+        op stk
         with Not_found -> print_string " **parse error\n";;
 
 (* top level parsing *)
@@ -82,8 +41,7 @@ let parse_line stk optab line =
 let read_loop optab =
         let stk = Stack.create() in
         while true do
-                dump_stack stk;
-                print_string("-> ");
+                Printf.printf "%3u -> " (Stack.length stk);
                 let x = try read_line() with
                         End_of_file -> print_string "\n"; exit 0 in
                 if String.length x != 0 then parse_line stk optab x
@@ -94,15 +52,31 @@ let op_swap stk = let o1 = Stack.pop stk in
         let o2 = Stack.pop stk in
         Stack.push o1 stk; Stack.push o2 stk;;
 
+(* Operations here. XXX move to separate compilation unit *)
+let op_eval_simple f stk =
+        let arg2 = Stack.pop stk in
+        let arg1 = Stack.pop stk in
+        let res = f arg1 arg2 in
+        Stack.push res stk;;
+
+let op_del stk = ignore (Stack.pop stk);;
+
+let op_swap stk =
+        let arg2 = Stack.pop stk in
+        let arg1 = Stack.pop stk in
+        Stack.push arg1 stk; Stack.push arg2 stk;;
+
 (* create and load up the operation mapping *)
 let init_optab () =
         let optab = ref StringMap.empty in
-        optab := StringMap.add "+" (BinaryOp Num.add_num) !optab;
-        optab := StringMap.add "-" (BinaryOp Num.sub_num) !optab;
-        optab := StringMap.add "*" (BinaryOp Num.mult_num) !optab;
-        optab := StringMap.add "/" (BinaryOp Num.div_num) !optab;
-        optab := StringMap.add "`" (SpecialOp (op_del, 1)) !optab;
-        optab := StringMap.add "s" (SpecialOp (op_swap, 2)) !optab;
+        optab := StringMap.add "+" (op_eval_simple Num.add_num) !optab;
+        optab := StringMap.add "p" dump_stack !optab;
+        optab := StringMap.add "+" (op_eval_simple Num.add_num) !optab;
+        optab := StringMap.add "-" (op_eval_simple Num.sub_num) !optab;
+        optab := StringMap.add "*" (op_eval_simple Num.mult_num) !optab;
+        optab := StringMap.add "/" (op_eval_simple Num.div_num) !optab;
+        optab := StringMap.add "`" op_del !optab;
+        optab := StringMap.add "s" op_swap !optab;
         !optab;;
 
 (* ---[ MAIN ] --- *)
