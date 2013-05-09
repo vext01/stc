04d3f947a28182b3e2c77725fd28112616dac2b3
vext01 <vext01@gmail.com>
2013-05-09 22:24:37 +0100

    allow nested unevaluated code

--- parse.ml
+++ parse.ml
@@ -7,17 +7,10 @@ let is_num s =
     | Failure "num_of_string" -> (false, (num_of_int 0)) in i
 
 let parse_unevaluated stk line = let len = String.length line in
-    if String.get line 0 = '<' && String.get line (len - 1) = '>' then (
+    if String.get line 0 = '<' && String.get line (len - 1) = '>' then
         let sub = String.sub line 1 (len - 2) in
-        let open_chev = try Some (String.index sub '<')
-            with Not_found -> None in
-            let close_chev = try Some (String.index sub '<')
-                with Not_found -> None in
-                if open_chev != None or close_chev != None then
-                    raise Parse_error
-                else
-                    Stack.push (Unevaluated sub) stk
-    ) else raise Parse_error
+            Stack.push (Unevaluated sub) stk
+    else raise Parse_error
 
 let parse_operator stk optab line =
     let op = try Some (OpMap.find line optab)
